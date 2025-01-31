b40ff89199028b11001f94152622f08d8058f556
Admin <Admin@TOUHID>
2025-01-31 21:04:13 +0600

    Add project files.

---
+++ OnlineBookShoop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineBookShoop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
