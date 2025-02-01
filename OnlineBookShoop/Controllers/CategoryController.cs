b8192455c51106c70047aa0fbf87e6e7aa0386fd
Admin <Admin@TOUHID>
2025-02-01 18:18:46 +0600

    adding serversite validation and client site validation.

--- OnlineBookShoop/Controllers/CategoryController.cs
+++ OnlineBookShoop/Controllers/CategoryController.cs
@@ -16,5 +16,30 @@ namespace OnlineBookShoop.Controllers
             List<Category> CategoryList = _db.Categories.ToList();
             return View(CategoryList);
         }
+
+        public IActionResult Create()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Name cannot exactly match the DisplayOrder");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+            
+        }
     }
 }
--- OnlineBookShoop/Models/Category.cs
+++ OnlineBookShoop/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineBookShoop.Models
 {
@@ -7,8 +8,12 @@ namespace OnlineBookShoop.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; } 
+        [Required]         //Server site Validation
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1-100") ]  //custom error messege
         public int DisplayOrder { get; set; }
     }
 }
