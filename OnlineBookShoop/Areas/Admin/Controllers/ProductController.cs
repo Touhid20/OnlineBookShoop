1cc5125d37f2af07987fe9737209c18ee26bc156
Admin <Admin@TOUHID>
2025-02-12 17:43:48 +0600

    Navigate entity by using Include property.

--- OnlineBookShoop/Areas/Admin/Controllers/ProductController.cs
+++ OnlineBookShoop/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace OnlineBookShoop.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> ProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> ProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
            
             return View(ProductList);
         }
